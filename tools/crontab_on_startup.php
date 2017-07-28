0b6284b117d37ceef8d19fcd15fca286740e44e8
Mikhail Sergeev <msergeev06@gmail.com>
2017-07-28 16:08:27 +0300

    Восстановление DB после уничтожения происходит в ядре а не при старте

--- tools/crontab_on_startup.php
+++ tools/crontab_on_startup.php
@@ -1,10 +1,8 @@
 <?php
 include_once('/var/www/kuzmahome/config.php');
 use MSergeev\Core\Lib as CoreLib;
-global $DB;
 
 $documentRoot = CoreLib\Config::getConfig('DOCUMENT_ROOT');
-$dirBackupDb = CoreLib\Config::getConfig('DIR_BACKUP_DB');
 
 //Устанавливаем флаг перезагрузки, чтобы автоматически запускаемые скрипты не исполнялись
 if (!file_exists($documentRoot.'reboot'))
@@ -14,44 +12,7 @@ if (!file_exists($documentRoot.'reboot'))
 	fclose($f1);
 }
 
-$issetDB=$DB->querySQL('SHOW DATABASES LIKE "'.CoreLib\Config::getConfig('DB_NAME').'"');
-if (!$issetDB)
-{
-	//Если базы данных нет - создаем базу и восстанавливаем данные из бекапа
-	$comm = $DB->getCreateDbCommand(CoreLib\Config::getConfig('DB_NAME'));
-	exec($comm);
-	$fileTime = null;
-	$filePath = null;
-	$dir=$dirBackupDb;
-	if (is_dir($dir)) {
-		if ($dh = opendir($dir)) {
-			while (($file = readdir($dh)) !== false) {
-				if (!is_dir($dir.$file) && $file != "." && $file != ".." && $file != ".htaccess")
-				{
-					if (strstr($file,CoreLib\Config::getConfig('DB_NAME'))!==false)
-					{
-						if (is_null($fileTime))
-						{
-							$fileTime = filemtime($dir.$file);
-							$filePath = $dir.$file;
-						}
-						elseif (filemtime($dir.$file) > $fileTime)
-						{
-							$fileTime = filemtime($dir.$file);
-							$filePath = $dir.$file;
-						}
-					}
-				}
-			}
-			closedir($dh);
-		}
-	}
-	if (!is_null($filePath))
-	{
-		$comm = $DB->getBackupCommand($filePath);
-		exec($comm);
-	}
-}
+CoreLib\Events::runEvents('kuzmahome','OnStartUp');
 
 //Убираем флаг перезагрузки
 if (file_exists($documentRoot.'reboot'))
@@ -59,15 +20,3 @@ if (file_exists($documentRoot.'reboot'))
 	unlink($documentRoot.'reboot');
 }
 
-
-if ($arEvents = CoreLib\Events::getPackageEvents('kuzmahome','OnStartUp'))
-{
-	foreach ($arEvents as $sort=>$ar_events)
-	{
-		foreach ($ar_events as $arEvent)
-		{
-			CoreLib\Events::executePackageEvent($arEvent);
-		}
-	}
-}
-//CoreLib\Events::runEvents('kuzmahome','OnStartUp');
--- tools/on_startup_handler.php
+++ tools/on_startup_handler.php
@@ -1,24 +1,8 @@
 <?php
 
-/*
-Все перенесено в метод объекта System
-
-use MSergeev\Packages\Kuzmahome\Lib;
-$scriptsDir = \MSergeev\Core\Lib\Config::getConfig('KUZMAHOME_ROOT').'scripts/';
-
-//Устанавливаем время включения, для рассчета аптайма
-\MSergeev\Core\Lib\Options::setOption('KUZMAHOME_STARTED_TIME',time());
-//Проверяем демонов, которые должны быть запущены при старте
-Lib\Daemons::checkDaemonsOnStartUp();
-
-//Сообщаем о готовности
-Lib\Say::say('Гот+ов к раб+оте',1);
-
-//Подключаем скрипт Проверки тарифа электроэнергии
-include($scriptsDir.'check_energo_tariff.php');
-*/
-$lastStartedTime = getGlobal('propStartedTime');
+$lastStartedTime = \MSergeev\Packages\Kuzmahome\Lib\Objects::getGlobal('System.propStartedTime');
 $nowTime = time();
+\MSergeev\Packages\Kuzmahome\Lib\Objects::setGlobal('System.propStartedTime',$nowTime);
 $lostTime = $nowTime - $lastStartedTime;
 if ($lostTime>3600)
 {
@@ -29,4 +13,4 @@ if ($lostTime>86400)
 	\MSergeev\Core\Lib\Events::runEvents('kuzmahome','OnNewDay');
 }
 
-callMethod('System.OnStartUp');
+\MSergeev\Packages\Kuzmahome\Lib\Objects::callMethod('System.OnStartUp');
